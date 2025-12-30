081b17f77e758410f10fdb8ddc84557978e5b66d
Maz2605 <vannam2654@gmail.com>
2025-12-31 02:04:41 +0700

    Maz message: Reset Project

---
+++ Assets/_Game/Core/Scripts/Data/SaveSystem.cs
@@ -0,0 +1,77 @@
+using System;
+using System.IO;
+using System.Text;
+using Newtonsoft.Json;
+using UnityEngine;
+
+namespace _Game.Core.Scripts.Data
+{
+    public class SaveSystem
+    {
+        private static bool USE_ENCRYPTION = true;
+
+        public static void Save<T>(string gameId, T data)
+        {
+            try
+            {
+                string filePath = GetPath(gameId);
+                string json = JsonConvert.SerializeObject(data, Formatting.Indented);
+                
+                if(USE_ENCRYPTION) json = Encrypt(json);
+
+                string tempPath = filePath + ".tmp";
+                File.WriteAllText(tempPath, json);
+                
+                if(File.Exists(filePath))
+                    File.Delete(filePath);
+                
+                File.Move(tempPath, filePath);
+                
+                Debug.Log($"[Save System] Saved: {gameId}");
+            }
+            catch (Exception e)
+            {
+                Debug.LogError($"[SaveSystem] Save ERROR: {e.Message}");
+            }
+        }
+        
+        public static T Load<T>(string gameId) where T : new()
+        {
+            string filePath = GetPath(gameId);
+            if (!File.Exists(filePath)) return new T(); 
+
+            try
+            {
+                string json = File.ReadAllText(filePath);
+                if (USE_ENCRYPTION) json = Decrypt(json);
+                return JsonConvert.DeserializeObject<T>(json);
+            }
+            catch (Exception e)
+            {
+                Debug.LogError($"[SaveSystem] Load Error: {e.Message}");
+                return new T(); 
+            }
+        }
+        
+        //Helpers
+
+        private static string GetPath(string gameId)
+        {
+            return Path.Combine(Application.persistentDataPath,$"{gameId}.json");
+        }
+
+        private static string Encrypt(string data)
+        {
+            var bytes = Encoding.UTF8.GetBytes(data);
+            return Convert.ToBase64String(bytes);
+        }
+
+        private static string Decrypt(string data)
+        {
+            var bytes = Convert.FromBase64String(data);
+            return Encoding.UTF8.GetString(bytes);
+        }
+        
+        
+    }
+}
---
+++ Assets/_Game/Core/Scripts/Utils/DesignPattern/Observer/ObserverManager.cs
@@ -0,0 +1,60 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace _Script.DesignPattern.ObsServer
+{
+    public static class ObserverManager<T> where T : Enum
+    {
+        private static readonly Dictionary<T, Action<object>> _eventDictionary = new();
+
+        public static void AddListener(T eventID, Action<object> callback)
+        {
+            if(callback == null)
+            {
+                Debug.LogWarning("Callback cannot be null.");
+                return;
+            }
+
+            if (!_eventDictionary.ContainsKey(eventID))
+            {
+                _eventDictionary[eventID] = delegate { };
+            }
+            
+            _eventDictionary[eventID] += callback;
+        }
+
+        public static void RemoveListener(T eventID, Action<object> callback)
+        {
+            if(callback == null) return;
+
+            if (_eventDictionary.ContainsKey(eventID))
+            {
+                _eventDictionary[eventID] -= callback;
+                
+                if(_eventDictionary[eventID] == null || _eventDictionary[eventID].GetInvocationList().Length == 0)
+                {
+                    _eventDictionary.Remove(eventID);
+                }
+            }
+        }
+        
+        public static void PostEvent(T eventID, object parameter = null)
+        {
+            if (_eventDictionary.TryGetValue(eventID, out var callback))
+            {
+                callback?.Invoke(parameter);
+            }
+            else
+            {
+                Debug.LogWarning($"No listeners for event: {eventID}");
+            }
+        }
+        
+        public static void ClearAllListeners()
+        {
+            _eventDictionary.Clear();
+            Debug.Log("All observers cleared.");
+        }
+    }
+}
